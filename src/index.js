5ee9deb728763bfa14dfe23235a27b7bc92ceb22
Ali Pirzada <alipirzada.khan@gmail.com>
2021-01-11 15:59:08 +0500

    initialize state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,81 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition();
+// Class Based Component
+class App extends React.Component {
+  // Method1 of initializing state through constructor function
+  // constructor(props) {
+  //   super(props);
 
-  return <div>Hi There!!!</div>;
-};
+  //   // THIS IS THE ONLY TIME we do direct assignment to this.state
+  //   this.state = { lat: null, errorMessage: "" };
+  // }
+
+  // This is an alternate way of initializing state outside constructor
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // IMP Note:- to update our state object we called setState!!!
+        this.setState({ lat: position.coords.latitude });
+
+        // We do not!!!
+        // this.state.lat = position.coords.latitude;
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("My component was just updated!!!- it rerendered!");
+  }
+
+  // React says we have to define render method!!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return (
+        <div className="ui container">
+          <div className="ui red message">
+            <i className="close icon"></i>
+            <div className="header">Unable to located</div>
+            <p> {this.state.errorMessage}</p>
+          </div>
+        </div>
+      );
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return (
+        <div className="ui container">
+          <div className="ui positive message">
+            <i className="close icon"></i>
+            <div className="header">Your Coordinates:</div>
+            <p>
+              Lat: {this.state.lat} Long:{this.state.long}{" "}
+            </p>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="ui container">
+          <div class="ui icon message">
+            <i class="notched circle loading icon"></i>
+            <div class="content">
+              <div class="header">Just one second</div>
+              <p>Hang Tight ! Loading your location! </p>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+if (module.hot) {
+  module.hot.accept();
+}
